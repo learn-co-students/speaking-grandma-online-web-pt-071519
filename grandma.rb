9c46c3f72978e86a5ccc982ca0454db834598a8c
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-06 08:07:42 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(greeting)
   if greeting == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
-   elsif greeting == greeting.downcase
-   return "HUH?! SPEAK UP, SONNY!"
- else
+   elsif greeting == greeting.upcase 
    return "NO, NOT SINCE 1938!"
-   
+ else
+   return "HUH?! SPEAK UP, SONNY!"
+ end
  end
 
 # Whatever you say to grandma, she should respond with
